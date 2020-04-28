94a6f6c6fed89adbe61531ce57c93255eca27c75
GGSEL <48293188+GGSEL@users.noreply.github.com>
2020-04-28 18:44:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ import asyncio
 import random
 from random import choice
 import config
-
-
+import time
 
 client = commands.Bot(command_prefix = '.')
 
+# ready bot
 @client.event
 async def on_ready():
 	print('BOT is ready')
@@ -23,14 +23,15 @@ async def on_member_join(member):
 	role = discord.utils.get(member.guild.roles, id = 697031247095922689)
 
 	await member.add_roles(role)
-	await channel.send(embed = discord.Embed(description = random.choice(config.join_messages).format(member.name), color = 0x0c0c0c ))
+	await channel.send(embed = discord.Embed(description = random.choice(config.join_messages).format(member.name), color = 0x00FF00 ))
 
-
+# disconnect users
 @client.event
 async def on_member_remove(member):
-	channel = client.get_channel( 704641304814026816 )
-	await channel.send(embed = discord.Embed(description = f'Пользователь ``{member.name}`` покинул сервер', color = 0xc05520 ))
+	channel = client.get_channel( 704641109103476806 )
+	await channel.send(embed = discord.Embed(description = f'Пользователь ``{member.name}`` покинул сервер', color = 0xFF0000 ))
 
+# messages
 @client.event 
 async def on_message(message):
 	await client.process_commands( message )
@@ -52,9 +53,10 @@ async def on_message(message):
 		if i in msg:
 			await message.delete()
 			await channel.send(f'Ай-ай-ай, {author.mention},материться тут нельзя!')
+			mute()
 
 
-
+#on edit
 @client.event
 async def on_message_edit(before, after):
 
@@ -64,67 +66,24 @@ async def on_message_edit(before, after):
 		if i in msg:
 			await after.delete()
 
-
-#clear command
-@client.command(pass_context = True)
-@commands.has_permissions(administrator = True)
-async def clear(ctx, amount = 100):
-	await ctx.channel.purge(limit = amount)
-
-#say hello
-@client.command(pass_context = True)
-async def hello(ctx, amount = 1):
-	await ctx.channel.purge(limit = amount)
-
-	author = ctx.message.author
-	await ctx.send(f'Привет, {author.mention}')
-
-@client.command(pass_context = True)
-async def bye(ctx, amount = 1):
-	await ctx.channel.purge(limit = amount)
-
-	author = ctx.message.author
-	await ctx.send(f'Пока, {author.mention}')
-
-@client.command(pass_context = True)
-@commands.has_permissions(administrator = True)
-async def bye_adm(ctx, member: discord.Member, amount = 1):
-	await ctx.channel.purge(limit = amount)
-
-	await ctx.send(f'Пока, {member.mention}')
-
-#privet message
-@client.command(pass_context = True)
-async def hello_prv(ctx, member: discord.Member, amount = 1):
-	await ctx.channel.purge(limit = amount)
-
-	author = ctx.author
-
-	await member.send(f'{member.name}, тебе привет от {author}')
-
-
 #kick
 @client.command(pass_context = True)
 @commands.has_permissions(administrator = True)
-async def kick(ctx, member: discord.Member, *, reason = None):
+async def kick(ctx, member: discord.Member):
 	await ctx.channel.purge(limit = 1)
 	channel = client.get_channel( 704643080036548618 )
 
-	await member.kick(reason = reason)
-	await channel.send(embed = discord.Embed(description = f'``{member.name}`` кикнут: {reason}', color = 0x9c5c0c ))
+	await member.kick()
+	await channel.send(embed = discord.Embed(description = random.choice(config.kick_message).format(member.mention), color = 0x9c5c0c ))
 
 
 #ban
 @client.command(pass_context = True)
 @commands.has_permissions(administrator = True)
-async def ban(ctx, member: discord.Member, *, reason = None):
+async def ban(ctx, member: discord.Member):
 	await ctx.channel.purge(limit = 1)
 	channel = client.get_channel( 704643080036548618 )
-
-	await member.ban(reason = reason)
-	await channel.send(embed = discord.Embed(description = f'``{member.name}`` забанен: {reason}', color = 0xc0c0fc ))
-
-	await member.send('Вы были забанены Ботом. Причина: ' + reason)
+	await channel.send(embed = discord.Embed(description = random.choice(config.ban_message).format(member.mention), color = 0xc0c0fc ))
 
 #unban
 @client.command(pass_context = True)
@@ -136,59 +95,51 @@ async def unban(ctx, *, member):
 
 	for ban_entry in banned_users:
 		user = ban_entry.user
-		channel = client.get_channel( 704643080036548618 )
+		channel = client.get_channel( 704681348979359745 )
 
 		await ctx.guild.unban(user)
-		await channel.send(embed = discord.Embed(description = f'``{member.name}`` разбанен: {reason}', color = 0x0c0c4c ))
+		await channel.send(embed = discord.Embed(description = random.choice(config.unban_message).format(member.mention), color = 0x0c0c4c ))
 
 		return
 
 #mute
 @client.command(pass_context = True)
 @commands.has_permissions(administrator = True)
-async def mute(ctx, member: discord.Member, reason = None, amount = 1):
+async def mute(ctx, member: discord.Member, amount = 1):
 	await ctx.channel.purge(limit = 1)
 
 	delete_r = discord.utils.get(member.guild.roles, id = 697031247095922689)
-	role     = discord.utils.get(member.guild.roles, id = 697455208372109383)
+	role     = discord.utils.get(member.guild.roles, id = 697456709651791973)
 
 	channel = client.get_channel( 704643080036548618 )
 
 	await member.remove_roles(delete_r)
 	await member.add_roles(role)
-	await channel.send(embed = discord.Embed(description = f'``{member.name}`` замучен: {reason}', color = 0x0c3c2c ))
-	
+	await channel.send(embed = discord.Embed(description = random.choice(config.mute_message).format(member.mention), color = 0x808080 ))
+
 # unmute
 @client.command(pass_context = True)
 @commands.has_permissions(administrator = True)
 async def unmute(ctx, member: discord.Member, amount = 1):
 	await ctx.channel.purge(limit = 1)
+	channel = client.get_channel( 704681348979359745 )
 
 	role       = discord.utils.get(member.guild.roles, id = 697031247095922689)
 	delete_r   = discord.utils.get(member.guild.roles, id = 697455208372109383)
 
 	await member.remove_roles(delete_r)
 	await member.add_roles(role)
+	await channel.send(embed = discord.Embed(description = random.choice(config.unmute_message).format(member.mention), color = 0x887764 ))
 
 #invite member to voice channel
 @client.command(pass_context = True)
-async def invite_vs(ctx, member: discord.Member):
+async def invite_vchat(ctx, member: discord.Member):
 	await ctx.channel.purge(limit = 1)
-	channel = ctx.channel
+	channel = client.get_channel( 695630701114032223 )
 	author = ctx.author
 
-	await channel.send(f'{member.mention} приглашаем тебя в голосовой чат!')
+	await channel.send(embed = discord.Embed(description = random.choice(config.invite_vchat_message).format(member.mention), color = 0x00FF00 ))
 	await member.send(f'{author}, приглашает Вас в голосовой чат!')
-
-#say by bot
-@client.command(pass_context = True)
-@commands.has_permissions(administrator = True)
-async def say_by_bot(ctx, message):
-	await ctx.channel.purge(limit = 1)
-
-	channel = ctx.channel
-
-	await channel.send(message)
 
 
 
